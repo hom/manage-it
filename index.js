9963acead69d618033368a9d5f3e4f3b20de4ec9
mengjun <813935532@qq.com>
2019-03-19 16:43:44 +0800

    modify index.js and add comment

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+// manage-it server side entry pointer
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+// create app instance
 let app = express();
 
+// parse body data
 app.use(express.json());
+app.use(express.urlencoded())
 
+// serve the static resource
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// default router entry
 app.use('/', (req, res) => {
   res.setHeader('Content-Type', 'text/html');
   fs.createReadStream(path.join(__dirname, 'dist', 'index.html')).pipe(res);
 })
 
+// The port of server listening on
 let port = process.env.MANAGE_IT_PORT || 10987;
 app.listen(port, () => {
   console.log(`Manage-it is running on port: ${port}`);
