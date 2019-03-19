fff5f154ac8844e96f7d3e43c141956babc9ac42
mengjun <813935532@qq.com>
2019-03-19 14:18:59 +0800

    modify .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,10 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': 'off',
+    'on-trailing-spaces': 'off',
+    'space-before-function-paren': 'off',
+    'comma-dangle': 'off',
   },
   parserOptions: {
     parser: 'babel-eslint',
