ffed47faa81d0088de819e6fd4383ca6edd4ed72
mengjun <813935532@qq.com>
2019-03-20 16:37:36 +0800

    add aside

--- src/stores/me.js
+++ src/stores/me.js
@@ -11,19 +11,22 @@ export default {
     },
   },
   actions: {
-    ACTION_CHECK_LOGIN({ commit }) {
-      if (localStorage.getItem('user')) {
-        commit('SET_ME', JSON.parse(localStorage.getItem('user')));
+    ACTION_CHECK_LOGIN({ state }) {
+      if (!state.me && !localStorage.getItem('CURRENT_USER')) {
+        return router.push('/login');
       }
-      router.push('/login');
+      return state.me;
     },
     ACTION_LOGIN({ commit }, form) {
       commit('SET_ME', form);
+      localStorage.setItem('CURRENT_USER', JSON.stringify(form));
+
       setTimeout(() => {
         router.push('/');
       }, 1000)
     },
     ACTION_LOGOUT({ commit }) {
+      localStorage.removeItem('CURRENT_USER')
       commit('SET_ME', '');
       setTimeout(() => {
         router.push('/login');
