77ee0555c6a19dfba9f44234d5672283f4c40a9a
mengjun <813935532@qq.com>
2019-07-01 16:26:50 +0800

    add: add login

--- src/stores/modules/me.js
+++ src/stores/modules/me.js
@@ -1,5 +1,6 @@
 import { Message } from 'element-ui';
 import router from '@/router';
+import api from '@/api';
 
 export default {
   namespaced: true,
@@ -18,14 +19,26 @@ export default {
       }
       return state.me;
     },
-    ACTION_LOGIN({ commit }, form) {
-      commit('SET_ME', form);
-      localStorage.setItem('CURRENT_USER', JSON.stringify(form));
+    async ACTION_LOGIN({ commit }, form) {
+      let result;
+      try {
+        result = await api.get('/parse/login', {
+          params: form,
+        });
+      } catch (error) {
+        console.error(error.response);
+        return Message.error('登录失败');
+      }
+      const user = result.data;
+      console.log(user);
+      commit('SET_ME', user);
+      localStorage.setItem('CURRENT_USER', JSON.stringify(user));
 
       Message.success('登陆成功');
       setTimeout(() => {
         router.push('/apps');
       }, 1000)
+      return user;
     },
     ACTION_LOGOUT({ commit }) {
       localStorage.removeItem('CURRENT_USER')
